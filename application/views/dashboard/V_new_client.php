1e9159f5bcedeaa7b4f608f1497bf5b680d1e9a2
Dedy ibrahim <dedyibrahym23@gmail.com>
2019-02-28 16:42:42 +0700

    ASD

--- application/views/dashboard/V_new_client.php
+++ application/views/dashboard/V_new_client.php
@@ -6,7 +6,7 @@
 
 <div class="data_content card p-2 m-3 ">
 <div class="row">
-<div class="col-md-6 mx-auto p-5">
+<div class="col-md-4 mx-auto p-2">
 <div class="text-center color_fa">
 <i class=" text-center fa-3x  fa fa-user-tie"></i>
 <h4>Create New Client</h4>
@@ -19,16 +19,143 @@
 <option value="Badan Hukum">Badan Hukum</option>	
 </select>
 </div>
+</div>
+<div style="display:none;" class="container" id="form_perorangan">
+<h1>PERORANGAN</h1>
 
 </div>
+<div style="display:none;" class="container" id="form_badan_hukum">
+<hr>
+<form  id="fileForm" method="post" action="<?php echo base_url('Dashboard/create_client') ?>">
+<div class="row">
+   
+<div class="col-md-4">
+<h5 align="center">Data Badan Hukum</h5><hr>
+<label>Jenis Akta</label>
+<input type="text" name="jenis_akta"  id="jenis_akta" class="form-control required"  accept="text/plain">
+<label>Nama Badan Hukum</label>
+<input type="text" name="badan_hukum" id="badan_hukum" class="form-control required"  accept="text/plain">
+<label>Alamat Badan Hukum</label>
+<textarea id="alamat_badan_hukum" class="form-control required"  accept="text/plain"></textarea>
+<hr>
+</div>
+<div class="col">
+    <button type="button" class="btn btn-success btn-sm float-right" id="tambah_perorangan">Tambah Perorangan <i class="fa fa-plus"></i></button>
+<h5 align="center">Data Perorangan</h5>
+<hr>
+<div class="data_orang skroll p-3">
+<div class="row hitung_orang">
+<div class="col">
+<label>Nama KTP</label>
+<input type="text" name="ktp1" id="ktp1"  class="form-control required" accept="text/plain" placeholder="Nama . . .">
+</div>
+<div class="col">
+<label>NIK KTP</label>
+<input type="text" name="nik1" id="nik1"  class="form-control required" accept="text/plain" placeholder="NIK . . .">
+</div>
+<div class="col">
+<label>Status</label>
+<select name="status1" id="status1" class="form-control required" accept="text/plain">
+<option>Direksi</option>
+<option>Komisaris</option>
+</select>
+</div>
+
+</div>    
+</div>
+</div>
+
+
+
+</div>
+<button type="submit" class="btn btn-success btn-block simpan_perizinan">Simpan & Proses Perizinan <i class="fa fa-save"></i></button>
+</form>
+</div>    
+
 </div>
 </div>
 </div>
 <script type="text/javascript">
 $("#pilih_jenis").on("change",function(){
-    
-alert("halo");    
-});    
-    
+var client = $("#pilih_jenis option:selected").text();
+if(client == "Perorangan"){
+$("#form_perorangan").show(100);   
+$("#form_badan_hukum").hide(100);    
+}else{
+$("#form_badan_hukum").show(100);       
+$("#form_perorangan").hide(100);   
+}
+
+});
+
+$("#tambah_perorangan").on("click",function(){
+var h = $(".hitung_orang").length+1;
+
+var data = "<div class='row hitung_orang'>\n\
+<div class='col'>\n\
+<label>Nama KTP</label>\n\
+<input type='text' id='ktp"+h+"' value='' class='form-control' placeholder='Nama . . .'></div>\n\
+<div class='col'>\n\
+<label>NIK KTP</label>\n\
+<input type='text' id='nik"+h+"' value='' class='form-control' placeholder='NIK . . .'>\n\
+</div>\n\
+<div class='col' >\n\
+<label>Status</label>\n\
+<select id='status"+h+"' class='form-control'>\n\
+<option>Direksi</option>\n\
+<option>Komisaris</option>\n\
+</select></div>\n\
+</div> ";
+$(data).appendTo('.data_orang').fadeIn( "slow", function() {
+});       
+});
+
+
 </script>        
+<script>
+$(document).ready(function() {
+
+$.validator.messages.required = '';
+$("#fileForm").validate({
+highlight: function (element, errorClass) {
+$(element).closest('.form-control').addClass('is-invalid');
+},
+unhighlight: function (element, errorClass) {
+$(element).closest(".form-control").removeClass("is-invalid");
+},submitHandler: function(form) {
+var data_orang = $(".hitung_orang").length+1;
+var values = [];
+
+for(i=1; i<data_orang; i++){
+values.push({
+ktp: $("#ktp"+i).val(),
+nik: $("#nik"+i).val(),
+status: $("#status"+i+" option:selected").text()
+});
+}
+
+var data = [
+{jenis_client       :$("#pilih_jenis option:selected").text()},
+{jenis_akta         :$("#jenis_akta").val()},
+{badan_hukum        :$("#badan_hukum").val()},
+{alamat_badan_hukum :$("textarea#alamat_badan_hukum").val()},
+values
+
+];
+var token    = "<?php echo $this->security->get_csrf_hash() ?>";
+
+    
+$.ajax({
+url: form.action,
+type: form.method,
+data: { 'token' : token,data},
+success: function(response) {
+$('#answers').html(response);
+}            
+});
+
+}
+});
+});
+</script>
 </body>
